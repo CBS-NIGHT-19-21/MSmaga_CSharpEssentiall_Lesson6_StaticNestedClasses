09d2b0e8a93ca52d822e11e155e4ddd515052b47
MSmaga <msmaga@protonmail.com>
2021-12-09 12:53:47 +0200

    added namespace, Program, Main for VS2019

--- Task4_Array_ExtensionMathod/Program.cs
+++ Task4_Array_ExtensionMathod/Program.cs
@@ -1,17 +1,27 @@
-﻿#region Задание 4
-//Используя Visual Studio, создайте проект по шаблону Console Application. 
-//Требуется: создать расширяющий метод для целочисленного массива, который сортирует элементы 
-//массива по возрастанию.
-#endregion
+﻿namespace Task4_Array_ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задание 4
+            //Используя Visual Studio, создайте проект по шаблону Console Application. 
+            //Требуется: создать расширяющий метод для целочисленного массива, который сортирует элементы 
+            //массива по возрастанию.
+            #endregion
 
-// Declaration and initialization of an array.
-int[] array = new[] { 2, 4, 6, 1, 2, 56, 34, 2, 95, 4, 2, 4, 13, 197, 1, 45, 2, 3, 59, 32, 44, 65, 45, 13, 19 };
+            // Declaration and initialization of an array.
+            int[] array = new[] { 2, 4, 6, 1, 2, 56, 34, 2, 95, 4, 2, 4, 13, 197, 1, 45, 2, 3, 59, 32, 44, 65, 45, 13, 19 };
 
-// Call extention method Show().
-array.Show();
+            // Call extention method Show().
+            array.Show();
 
-// Call extention method SortInc().
-array.SortInc();
+            // Call extention method SortInc().
+            array.SortInc();
 
-// Delay.
-Console.ReadLine();
+            // Delay.
+            Console.ReadLine();
+        }
+    }
+}
+
